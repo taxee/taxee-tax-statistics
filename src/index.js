d1d9552c49f73414de07ccbe56573e1e7ce4c21b
taxee <support@taxee.io>
2016-12-30 16:00:52 -0500

    Now properly exporting 2017.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import y2014 from './statistics/2014/index.js';
 import y2015 from './statistics/2015/index.js';
 import y2016 from './statistics/2016/index.js';
+import y2017 from './statistics/2017/index.js';
 
 export default {
   '2014': y2014,
   '2015': y2015,
-  '2016': y2016
+  '2016': y2016,
+  '2017': y2017
 }
