524891ff8af6db5df0e11d6be74d1ac2881c8fd5
Andy Hass <andyhass@cmgx.io>
2020-01-04 16:31:45 -0600

    WIP

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import y2014 from './statistics/2014/index.js';
-import y2015 from './statistics/2015/index.js';
-import y2016 from './statistics/2016/index.js';
-import y2017 from './statistics/2017/index.js';
-import y2018 from './statistics/2018/index.js';
-import y2019 from './statistics/2019/index.js';
+import y2014 from "./statistics/2014/index.js";
+import y2015 from "./statistics/2015/index.js";
+import y2016 from "./statistics/2016/index.js";
+import y2017 from "./statistics/2017/index.js";
+import y2018 from "./statistics/2018/index.js";
+import y2019 from "./statistics/2019/index.js";
+import y2020 from "./statistics/2020/index.js";
 
 export default {
-  '2014': y2014,
-  '2015': y2015,
-  '2016': y2016,
-  '2017': y2017,
-  '2018': y2018,
-  '2019': y2018
-}
+  "2014": y2014,
+  "2015": y2015,
+  "2016": y2016,
+  "2017": y2017,
+  "2018": y2018,
+  "2019": y2019,
+  "2020": y2020
+};
